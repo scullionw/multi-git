1f563e32b6c428bee9de0b5ed2041ac89dee41d5
scullionw <williamscullion@gmail.com>
2019-05-10 15:24:24 -0400

    Refactor, added sync status, added readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Installation
+    cargo install --git "https://github.com/scullionw/multi-git"
+    
+# Example script on windows (check.bat):
+    mgit "C:\Users\User\Desktop\Projects"
+    pause
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::env;
 use std::fs;
 use std::io::Write;
 use std::path::{Path, PathBuf};
+use std::process::Command;
 use structopt::StructOpt;
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
@@ -12,63 +13,102 @@ fn main() {
     let target = config.target_dir.as_ref().unwrap_or(&current_dir).as_path();
 
     if target.is_dir() {
-        match git_status(target) {
-            DirectoryStatus::NOTAREPO => {
+        match Repository::open(target) {
+            Ok(repo) => repo.show(),
+            Err(_) => {
                 for entry in fs::read_dir(target).unwrap() {
-                    let entry = entry.unwrap().path();
-                    check(&entry);
+                    let path = entry.unwrap().path();
+                    match Repository::open(&path) {
+                        Ok(repo) => repo.show(),
+                        Err(_) => println!("{}", extract_name(path.as_path())),
+                    }
                 }
             }
-            _ => check(target),
         }
     } else {
         panic!("{} is not a directory!", target.display());
     }
 }
 
-enum DirectoryStatus {
-    OK,
-    NOTOK,
-    NOTAREPO,
+fn extract_name(path: &Path) -> &str {
+    path.iter().last().unwrap().to_str().unwrap()
 }
 
-fn check(path: impl AsRef<Path>) {
-    let name = path.as_ref().iter().last().unwrap().to_str().unwrap();
-    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+enum RepoStatus {
+    Clean,
+    Dirty,
+}
 
-    match git_status(&path) {
-        DirectoryStatus::OK => {
+trait StatusExt {
+    fn is_synced(&self) -> bool;
+    fn status(&self) -> RepoStatus;
+    fn show(&self);
+}
+
+impl StatusExt for Repository {
+    fn is_synced(&self) -> bool {
+        let mut git_path = self.path().to_path_buf();
+        git_path.push(".git");
+        let git_path = git_path.as_path().to_str().unwrap();
+        let output = Command::new("git")
+            .args(&[
+                "--git-dir",
+                git_path,
+                "log",
+                "--branches",
+                "--not",
+                "--remotes",
+            ])
+            .output()
+            .unwrap();
+
+        output.stdout.is_empty()
+    }
+
+    fn status(&self) -> RepoStatus {
+        let mut opts = StatusOptions::new();
+        opts.include_ignored(false);
+        opts.include_untracked(true).recurse_untracked_dirs(true);
+        let statuses = self.statuses(Some(&mut opts)).unwrap();
+        if statuses.iter().count() == 0 {
+            RepoStatus::Clean
+        } else {
+            RepoStatus::Dirty
+        }
+    }
+
+    fn show(&self) {
+        let mut stdout = StandardStream::stdout(ColorChoice::Always);
+        match self.status() {
+            RepoStatus::Clean => {
+                stdout
+                    .set_color(ColorSpec::new().set_fg(Some(Color::Green)))
+                    .unwrap();
+                write!(&mut stdout, "Clean ").unwrap();
+            }
+            RepoStatus::Dirty => {
+                stdout
+                    .set_color(ColorSpec::new().set_fg(Some(Color::Red)))
+                    .unwrap();
+                write!(&mut stdout, "Dirty ").unwrap();
+            }
+        }
+
+        if self.is_synced() {
             stdout
                 .set_color(ColorSpec::new().set_fg(Some(Color::Green)))
                 .unwrap();
-            write!(&mut stdout, "OK: ").unwrap();
-        }
-        DirectoryStatus::NOTOK => {
+            write!(&mut stdout, "Synced ").unwrap();
+        } else {
             stdout
-                .set_color(ColorSpec::new().set_fg(Some(Color::Red)))
+                .set_color(ColorSpec::new().set_fg(Some(Color::Blue)))
                 .unwrap();
-            write!(&mut stdout, "NEEDS CARE: ").unwrap();
+            write!(&mut stdout, "PUSH ").unwrap();
         }
-        DirectoryStatus::NOTAREPO => (),
-    }
-    stdout.reset().unwrap();
-    println!("{}", name)
-}
 
-fn git_status(path: impl AsRef<Path>) -> DirectoryStatus {
-    match Repository::open(path) {
-        Ok(repo) => {
-            let mut opts = StatusOptions::new();
-            opts.include_ignored(false);
-            opts.include_untracked(true).recurse_untracked_dirs(true);
-            let statuses = repo.statuses(Some(&mut opts)).unwrap();
-            if statuses.iter().count() == 0 {
-                DirectoryStatus::OK
-            } else {
-                DirectoryStatus::NOTOK
-            }
-        }
-        Err(_) => DirectoryStatus::NOTAREPO,
+        stdout.reset().unwrap();
+        let name = extract_name(self.workdir().unwrap());
+        println!("{}", name);
     }
 }
 
